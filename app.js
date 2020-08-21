c767b0fe71d4e71e5df2dd911ffa55676ea5fdf9
Timo_dev <30313199+timotismjntk@users.noreply.github.com>
2020-08-21 07:51:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const penghitungJarak = require('./src/function.js') //import function.js
 
 
-const hasil = new penghitungJarak("10:12:21", "12:00:00", 6)
+const hasil = new penghitungJarak("10:12:21", "12:00:00", 6) // penghitungJarak(pukulAwal, pukulAkhir, kecepatanAwal)
 hasil.mulai()
